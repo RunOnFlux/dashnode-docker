e3001ebdfd78e2f3e484f8e013bf5fd5a630c102
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-10-31 09:26:26 +0100

    Create node_initialize.sh

---
+++ node_initialize.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+function get_ip() {
+
+    WANIP=$(curl --silent -m 15 https://api4.my-ip.io/ip | tr -dc '[:alnum:].')
+
+    if [[ "$WANIP" == "" ]]; then
+      WANIP=$(curl --silent -m 15 https://checkip.amazonaws.com | tr -dc '[:alnum:].')
+    fi
+
+    if [[ "$WANIP" == "" ]]; then
+      WANIP=$(curl --silent -m 15 https://api.ipify.org | tr -dc '[:alnum:].')
+    fi
+}
+
+
+get_ip
+RPCUSER=$(pwgen -1 8 -n)
+PASSWORD=$(pwgen -1 20 -n)
+
+if [[ -f /root/.dashcore/dash.conf ]]; then
+  rm  /root/.dashcore/dash.conf
+fi
+
+touch /root/.dashcore/dash.conf
+cat << EOF > /root/.dashcore/dash.conf
+rpcuser=$RPCUSER
+rpcpassword=$PASSWORD
+rpcallowip=127.0.0.1
+listen=1
+server=1
+daemon=1
+externalip=$WANIP
+masternodeblsprivkey=$KEY
+maxconnections=256
+EOF
+
+while true; do
+dashd
+sleep 60
+done

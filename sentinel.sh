5933781ee81593cbfcd6a0cff80f31f19b5e26e1
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-10-31 11:18:20 +0100

    Update sentinel.sh

--- sentinel.sh
+++ sentinel.sh
@@ -13,6 +13,8 @@ if [[ ! -f /tmp/crone ]]; then
     sleep 120
     echo -e "Added crone job for fill as gaps..."
     (crontab -l -u "$USER" 2>/dev/null; echo "* * * * * cd /root/.dashcore/sentinel && ./venv/bin/python bin/sentinel.py  2>&1 >> sentinel-cron.log") | crontab -
+    (crontab -l -u "$USER" 2>/dev/null; echo "* * * * * pidof dashd || /root/.dashcore/dashd") | crontab -
+   
     echo -e "Cron job added!" >> /tmp/crone
  else
     echo -e "Cron job already exist..."

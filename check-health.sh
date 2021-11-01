f4f96ef5970acc290cf4058d2e7a1b8fa583ae1d
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-11-01 14:29:47 +0100

    added block height

--- check-health.sh
+++ check-health.sh
@@ -33,7 +33,7 @@ else
 fi
 
 if [[ "$DIFF" -le 10 ]]; then
- echo "Daemon working and is synced with network"
+ echo "Daemon working and is synced with network (block height: $CURRENT_NODE_HEIGHT)"
 else
  echo "Daemon working but is not synced with network"
  exit 1
